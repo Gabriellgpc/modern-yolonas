18bdf307ca333b6eb2e350e76dca6018686e823a
Luis Condados <condadoslgpc@gmail.com>
2026-02-05 20:32:34 -0400

    Update ONNX dependencies to include onnxscript and bump version to 0.1.dev5

--- src/modern_yolonas/_version.py
+++ src/modern_yolonas/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev2+g96a88fabc.d20260205'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'g96a88fabc.d20260205')
+__version__ = version = '0.1.dev5+gf0fc48f5a.d20260206'
+__version_tuple__ = version_tuple = (0, 1, 'dev5', 'gf0fc48f5a.d20260206')
 
 __commit_id__ = commit_id = None
